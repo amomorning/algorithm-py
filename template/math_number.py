231a16331373981efb94a367107ca5b259c81b4c
Yichen Mo <amomorning@gmail.com>
2022-06-30 10:38:12 +0200

    Use global MOD

--- template/math_number.py
+++ template/math_number.py
@@ -1,41 +1,41 @@
 # closed range
 
 def crange(start, end, step=1):
-    dir = 1 if start > end else -1
-    if start > end and step > 0:
-        step = -step
+    dir = 1 if start < end else -1
+    if start > end and step > 0: step = -step
     return range(start, end + dir, step)
 
 
 # quick power
-def qpow(a, b, mod):
+MOD = 998244353
+def qpow(a, b):
     ans = 1
     while b:
-        if b & 1: ans = ans * a % mod
-        a = a * a % mod; b >>= 1
+        if b & 1: ans = ans * a % MOD
+        a = a * a % MOD; b >>= 1
     return ans
 
  
 # binomial
+MOD = 998244353
 class Binomial:
-    def __init__(self, n, mod):
-        n = min(n, mod)
-        self.mod = mod
+    def __init__(self, n):
+        n = min(n, MOD)
         self.fact = [1, 1]
         self.inv_fact = [1, 1]
         self.inv = [0, 1]
         
         for i in crange(2, n):
-            self.fact.append(self.fact[-1] * i % mod)
-            self.inv.append((mod - mod // i) * self.inv[mod % i] % mod)
-            self.inv_fact.append(self.inv_fact[-1] * self.inv[-1] % mod)
+            self.fact.append(self.fact[-1] * i % MOD)
+            self.inv.append((MOD - MOD // i) * self.inv[MOD % i] % MOD)
+            self.inv_fact.append(self.inv_fact[-1] * self.inv[-1] % MOD)
     
 
     def comb(self, n, m):
         if m < 0 or m > n:
             return 0
         m = min(m, n-m)
-        return self.fact[n] * self.inv_fact[m] * self.inv_fact[n-m] % self.mod
+        return self.fact[n] * self.inv_fact[m] * self.inv_fact[n-m] % MOD
 
     ''' Lucas theorem
     - mod should be less than 1e5
@@ -43,28 +43,28 @@ class Binomial:
     def lucas(self, n, m): 
         if m == 0:
             return 1
-        return self.comb(n % self.mod, m % self.mod) * \
-            self.lucas(n // self.mod, m // self.mod) % self.mod
+        return self.comb(n % MOD, m % MOD) * \
+            self.lucas(n // MOD, m // MOD) % MOD
 
-def norm(v, mod):
-    v = v % mod
-    if v < 0: v += mod
+MOD = 998244353
+def norm(v):
+    v = v % MOD
+    if v < 0: v += MOD
     return v
 
 class Integral:
-    def __init__(self, v, mod = 998244353):
-        self.mod = mod
-        self.v = norm(v, mod)
+    def __init__(self, v):
+        self.v = norm(v, MOD)
 
     def __str__(self):
         return str(self.v)
     
     def __neg__(self):
-        return Integral(-self.v, self.mod)
+        return Integral(-self.v, MOD)
 
     def __iadd__(self, rhs):
         if type(rhs) == Integral: rhs = rhs.v
-        self.v = norm(self.v + rhs, self.mod)
+        self.v = norm(self.v + rhs, MOD)
         return self
     
     def __isub__(self, rhs):
@@ -72,30 +72,30 @@ class Integral:
         return self
         
     def __imul__(self, rhs):
-        if type(rhs) == int: rhs = Integral(rhs, self.mod)
-        self.v = norm(self.v * rhs.v, self.mod)
+        if type(rhs) == int: rhs = Integral(rhs, MOD)
+        self.v = norm(self.v * rhs.v, MOD)
         return self
     
     def __itruediv__(self, rhs):
-        if type(rhs) == int: rhs = Integral(rhs, self.mod)
-        self.v = norm(self.v * rhs.inv().v, self.mod)
+        if type(rhs) == int: rhs = Integral(rhs, MOD)
+        self.v = norm(self.v * rhs.inv().v, MOD)
         return self
     
     def copy(self):
-        return Integral(self.v, self.mod)
+        return Integral(self.v, MOD)
     
     def pow(self, b):
         assert type(b) == int
         ret, a = 1, self.v
         while b > 0:
             if b & 1:
-                ret = ret * a % self.mod
+                ret = ret * a % MOD
             b >>= 1
-            a = a * a % self.mod
-        return Integral(ret, self.mod)
+            a = a * a % MOD
+        return Integral(ret, MOD)
     
     def inv(self):
-        return self.pow(self.mod-2)
+        return self.pow(MOD-2)
     
     def __add__(self, rhs):
         ret = self.copy(); ret += rhs
@@ -115,11 +115,11 @@ class Integral:
     
     def __lshift__(self, rhs):
         if type(rhs) == Integral: rhs = rhs.v
-        return Integral(self.v << rhs, self.mod)
+        return Integral(self.v << rhs, MOD)
 
     def __rshift__(self, rhs):
         if type(rhs) == Integral: rhs = rhs.v
-        return Integral(self.v >> rhs, self.mod)
+        return Integral(self.v >> rhs, MOD)
     
     def __pow__(self, rhs):
         return self.pow(rhs)
