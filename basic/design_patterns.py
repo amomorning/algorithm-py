641ab6c967d54423fb73acf8e23ab9f044589319
Yichen Mo <amomorning@gmail.com>
2023-08-02 16:30:11 +0800

    Add template method

--- basic/design_patterns.py
+++ basic/design_patterns.py
@@ -423,6 +423,115 @@ invoker.delete_file()
 invoker.undo_all()
 
 
+# Template Method
+# allow subclasses to redefine certain steps of an algorithm without changing the algorithm's structure
+# - abstract class: define the algorithm's skeleton
+# - concrete class: implement the abstract class's abstract methods
+import urllib.request
+from xml.dom import minidom
+
+class AbstractNewsParser(metaclass=ABCMeta):
+    def __init__(self):
+        if self.__class__ is AbstractNewsParser:
+            raise TypeError('abstract class cannot be instantiated')
+
+    def print_top_news(self):
+        url = self.get_url()
+        raw_content = self.get_raw_content(url)
+        content = self.parse_content(raw_content)
+        cropped = self.crop_content(content)
+
+        for item in cropped:
+            print('Title: ', item['title'])
+            print('Content: ', item['content'])
+            print('Link: ', item['link'])
+            print('Published: ', item['published'])
+            print('Id: ', item['id'])
+    
+    def get_url(self):
+        raise NotImplementedError
+    
+    def get_raw_content(self, url):
+        return urllib.request.urlopen(url).read()
+
+    def parse_content(self, content):
+        raise NotImplementedError
+    
+    def crop_content(self, parsed_content, max_items=3):
+        return parsed_content[:max_items]
+    
+
+class GoogleNewsParser(AbstractNewsParser):
+    def get_url(self):
+        return 'https://news.google.com/news/feeds?output=rss'
+    
+    def parse_content(self, raw_content):
+        parsed_content = []
+        dom = minidom.parseString(raw_content)
+        for node in dom.getElementsByTagName('item'):
+            parsed_item = {}
+            try:
+                parsed_item['title'] = node.getElementsByTagName('title')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['title'] = None
+            try:
+                parsed_item['content'] = node.getElementsByTagName('description')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['content'] = None
+            try:
+                parsed_item['link'] = node.getElementsByTagName('link')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['link'] = None
+            try:
+                parsed_item['published'] = node.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['published'] = None
+            try:
+                parsed_item['id'] = node.getElementsByTagName('guid')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['id'] = None
+            parsed_content.append(parsed_item)
+        return parsed_content
+
+
+class YahooNewsParser(AbstractNewsParser):
+    def get_url(self):
+        return 'https://news.yahoo.com/rss/'
+    
+    def parse_content(self, raw_content):
+        parsed_content = []
+        dom = minidom.parseString(raw_content)
+        for node in dom.getElementsByTagName('item'):
+            parsed_item = {}
+            try:
+                parsed_item['title'] = node.getElementsByTagName('title')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['title'] = None
+            try:
+                parsed_item['content'] = node.getElementsByTagName('media:content')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['content'] = None
+            try:
+                parsed_item['link'] = node.getElementsByTagName('link')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['link'] = None
+            try:
+                parsed_item['published'] = node.getElementsByTagName('pubDate')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['published'] = None
+            try:
+                parsed_item['id'] = node.getElementsByTagName('guid')[0].childNodes[0].nodeValue
+            except IndexError:
+                parsed_item['id'] = None
+            parsed_content.append(parsed_item)
+        return parsed_content
+
+google = GoogleNewsParser()
+yahoo = YahooNewsParser()
+
+print('Google: ', google.print_top_news())
+print('Yahoo: ', yahoo.print_top_news())
+
 # Model View Controller
 # popular in web development, such as web2py, Pyramid, Django
 # - Model: data access layer
